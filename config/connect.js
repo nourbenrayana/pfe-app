62d771fc33f78056275d6b8b2b1759c8ba8ea176
Nour ben rayana <166948954+nourbenrayana@users.noreply.github.com>
2025-02-17 10:19:40 +0100

    Delete config directory

--- config/connect.js
+++
@@ -1,7 +0,0 @@
-const { DocumentStore } = require("ravendb");
-
-const store = new DocumentStore("https://a.nour.ravendb.community/", "BD");
-
-store.initialize();
-
-console.log("Connected to RavenDB!");
